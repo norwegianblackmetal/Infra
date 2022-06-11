301942ef1386dad3061695048afca51f5f682f09
norwegianblackmetal <finojenok.m@gmail.com>
2022-06-11 19:17:24 +0300

    .

---
+++ hyperv_vm_init.sh
@@ -0,0 +1,9 @@
+##############################
+#                            #
+#  This script is initial    #
+#  new HYPER-V machine       #
+#                            #
+##############################
+
+yum -y install hyperv-daemons
+yum -y install hyperv-tools
