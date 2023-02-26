913938d03236b770182c39968e38adfe7a233b64
norwegianblackmetal <mfinozhenok@protonmail.com>
2023-02-26 20:52:35 +0300

    Best changes ever

--- haproxy/START.sh
+++ haproxy/START.sh
@@ -3,4 +3,4 @@ do
     podman run --rm -d -p 808$i:80 httpd:alpine3.17 sh -c "hostname > /usr/local/apache2/htdocs/index.html; httpd-foreground"
 done
 
-podman run --rm -d -p 8080:80 -v "haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg" haproxy:2.7.3
+podman run --rm -d --network host -v "./haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg" haproxy:2.7.3
